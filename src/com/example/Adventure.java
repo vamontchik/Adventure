3667e257458dcbb77c75131c2d1d172361d228e9
Craig Zilles <zilles@illinois.edu>
2017-09-21 12:58:46 -0500

    Initial commit

---
+++ src/com/example/Adventure.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Adventure {
+
+    public static void main(String[] args) {
+
+        // you can remove this code; it is just illustrating the use of arguments.
+        System.out.print("The contents of my arguments are: ");
+
+        // this is a 'for each' loop; they are useful when you want to do something to
+        // every element of a collection and you don't care about the index of the element
+        for (String arg : args) {
+            System.out.print("\"" + arg + "\" ");
+        }
+
+        System.out.println("");
+        // you can remove this code; it is just illustrating the use of arguments.
+    }
+}
